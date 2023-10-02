bbacb3f066ca94042e51c6281e1fb031f7dd407d
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-02 18:14:33 +0200

    Created Login page

---
+++ PHP Scripts/Project/Login.php
@@ -0,0 +1,15 @@
+<html>
+
+    <head>
+
+        <title> Login </title>
+        <link rel="stylesheet" href="styles.css">
+
+    </head>
+
+    <?php include('Header.php'); ?>
+
+    <body>
+        
+    </body>
+?>
