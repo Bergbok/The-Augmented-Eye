e47eef993ff2ce1768b428ffa76d4287b0d0d456
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-02 18:29:40 +0200

    Created Login page layout

--- PHP Scripts/Project/Login.php
+++ PHP Scripts/Project/Login.php
@@ -10,6 +10,28 @@
     <?php include('Header.php'); ?>
 
     <body>
-        
+        <div class="center pixel-text">
+            <form method="POST">
+                <fieldset>
+
+                    <legend align="center">
+                        <strong>Login</strong>
+                    </legend>
+
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="newuser_Name"></input>
+
+                    <br/><br/>
+
+                    <label for="surname">Surname:</label>
+                    <input type="text" id="surname" name="newuser_Surname"></input>
+
+                    <br/><br/>
+
+                    <input class="submit-button" type="submit" value="Register"></input>
+                </fieldset>
+            </form>
+        </div>
     </body>
-?>
+
+</html>
