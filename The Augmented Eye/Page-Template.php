1f151f33b38bbee385b6d1fb67cd782ba9dc4f5f
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-04 16:45:54 +0200

    Created Page Template

---
+++ The Augmented Eye/Page-Template.php
@@ -0,0 +1,18 @@
+<?php
+    include('Header.php'); 
+?>
+
+<html>
+
+    <head>
+
+        <title> TITLE-GOES-HERE </title>
+        <link rel="stylesheet" href="styles.css">
+
+    </head>
+
+    <body>
+
+    </body>
+
+</html>
