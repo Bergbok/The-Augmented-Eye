552329fce350a99e08d5e9aff74dbd36e33e5431
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-07 18:55:59 +0200

    Made staff titles bold in About.php

--- The Augmented Eye/About.php
+++ The Augmented Eye/About.php
@@ -28,7 +28,7 @@
                     <hr>
                     <figcaption class='centered-text'><a href='/The Augmented Eye/Profile?profileID=3'> Donovan D. Dawson </a></figcaption>
                     <hr>
-                    <p class='centered-text'> Owner and CEO of The Augmented Eye </p>
+                    <b><p class='centered-text'> Owner and CEO of The Augmented Eye </p></b>
                     <p style='padding:15px;'> Donovan has a very crude personality and is often one to speak his mind. He states that he cheats on his wife almost as much as she cheats on him </p>
                 </figure>
                 <br>
@@ -37,14 +37,14 @@
                     <hr>
                     <figcaption class='centered-text'><a href='/The Augmented Eye/Profile?profileID=1'> Kimberly La Vallete </a></figcaption>
                     <hr>
-                    <p class='centered-text'> Writer </p>
+                    <b><p class='centered-text'> Writer </p></b>
                     <p style='padding:15px;'> Kim is a somewhat short woman with tanned skin and red eyes. Exclusively wears a white striped turtle-neck jumper and red-rimmed glasses. <br><br> Journalism wasn't her first preference in college, it was robotics, but since she couldn't get into the course, she settled for journalism. </p>
                 </figure>
                 <br>
                 <figure id='lana' class='center staff-border'>
                     <figcaption style='padding-top: 10px;' class='centered-text'><a href='/The Augmented Eye/Profile?profileID=2'> Lana Smithee </a></figcaption>
                     <hr>
-                    <p class='centered-text'> Writer(s) </p>
+                    <b><p class='centered-text'> Writer(s) </p></b>
                     <p style='padding:15px;'> You don't really think Lana Smithee is just one person, do you? - <a href='/The Augmented Eye/Profile?profileID=3'> Donovan D. Dawson </a></p>
                 </figure>
                 <br>
